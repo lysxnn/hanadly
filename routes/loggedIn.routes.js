7e4dc5dec2b56d7bc2db5b84ddc7b0f97bf39413
Lysann Kuettner <ly.sann@live.de>
2022-03-24 13:45:37 +0100

    changes layout

--- routes/loggedIn.routes.js
+++ routes/loggedIn.routes.js
@@ -1,35 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const eventmodel = require("../models/Event.model");
 
 const isLoggedIn = require("../middleware/isLoggedIn");
 
 router.get("/profile", isLoggedIn, (req, res) => {
-  console.log("this is the user", req.session.user);
   res.render("auth/profile", { user: req.session.user });
-});
-
-//TODO: isLoggedIn noch einfügen
-router.get("/create-event", (req, res) => {
-  res.render("auth/eventform", { user: req.session.user });
-});
-
-/* POST create-event page */
-router.post("/create-event", isLoggedIn, async (req, res, next) => {
-  const { eventname, date, eventtype, description, contact, email } = req.body;
-  const eventowner = req.session.user._id;
-  let createdEvent = {
-    eventname,
-    date,
-    eventtype,
-    description,
-    contact,
-    email,
-    eventowner,
-  };
-  const eventFromDB = await eventmodel.create(createdEvent);
-  console.log("eventfrom DB", eventFromDB);
-  res.redirect("/profile");
 });
 
 router.post("/logout", isLoggedIn, (req, res) => {
