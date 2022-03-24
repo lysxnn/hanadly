9d0347c47de63dc4323a4edfd97f0b9e8ed416a9
Lysann Kuettner <ly.sann@live.de>
2022-03-24 14:24:43 +0100

    created concept-route

--- routes/loggedIn.routes.js
+++ routes/loggedIn.routes.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const conceptmodel = require("../models/Concept.model")
 
 const isLoggedIn = require("../middleware/isLoggedIn");
 
 router.get("/profile", isLoggedIn, (req, res) => {
   res.render("auth/profile", { user: req.session.user });
+});
+
+router.get("/create-concept", (req, res) => {
+  res.render("auth/conceptform");
+});
+
+router.post("/create-concept", isLoggedIn, async (req, res, next) => {
+  const { concept, description, contact, email } = req.body;
+  const conceptowner = req.session.user._id;
+  let createdConcept = {
+    concept,
+    description,
+    contact,
+    email,
+    conceptowner,
+  };
+  const conceptFromDB = await conceptmodel.create(createdConcept);
+  console.log("conceptfrom DB", conceptFromDB);
+  res.redirect("/profile");
 });
 
 router.post("/logout", isLoggedIn, (req, res) => {
